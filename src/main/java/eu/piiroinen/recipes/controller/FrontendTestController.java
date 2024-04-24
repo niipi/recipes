f432097b508ba4c42c9c60599baba6478b37377b
Niina <niina@piiroinen.eu>
2024-04-24 12:02:23 +0300

    Still attempting to configure CORS for frontent test endpoints, to be tested

--- src/main/java/eu/piiroinen/recipes/controller/FrontendTestController.java
+++ src/main/java/eu/piiroinen/recipes/controller/FrontendTestController.java
@@ -26,7 +26,7 @@ public class FrontendTestController {
 
     private FrontendTestCategoryGenerator testCategoryGenerator = new FrontendTestCategoryGenerator();
 
-    @CrossOrigin(origins="https://localhost:3000")
+    @CrossOrigin(origins="https://localhost:3000", allowedHeaders = "*")
     @RequestMapping(value="/frontendtest")
     public ResponseEntity<Map<String, List<Recipe>>> getTestRecipeListing() {
         try {
@@ -54,7 +54,7 @@ public class FrontendTestController {
         }
     }
 
-    @CrossOrigin(origins="https://localhost:3000")
+    @CrossOrigin(origins="https://localhost:3000", allowedHeaders = "*")
     @RequestMapping(value="/frontendcategorytest")
     public ResponseEntity<Map<String, List<RecipeCategory>>> getTestCategoryListing() {
         try {
